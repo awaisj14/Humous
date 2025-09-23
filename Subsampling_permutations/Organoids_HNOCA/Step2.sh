0c73f35ff21e3a24c71266db57da0c49a45298d8
awaisj14 <65031858+awaisj14@users.noreply.github.com>
2025-09-23 10:34:39 +0200

    Create Step2.sh

---
+++ Subsampling_permutations/Organoids_HNOCA/Step2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=hm_int
+#SBATCH --output=logs/perm_%A_%a.out
+#SBATCH --error=logs/perm_%A_%a.err
+#SBATCH --array=1-5
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=shared-bigmem
+#SBATCH --mem=200G
+#SBATCH --time=08:00:00
+
+singularity exec \
+  /acanas/m-BioinfoSupport/singularity/ngs_v1.1.sif \
+  Rscript Step2.R perm_${SLURM_ARRAY_TASK_ID}
+
+
